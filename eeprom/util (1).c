a57c43912137a569a707aad46f5fba7dcb68ab5c
lewi <alli.faiye@gmail.com>
2026-07-20 14:57:06 -0400

    Update header comments in util.c
    Removed course and assignment details from the header.

--- eeprom/util (1).c
+++ eeprom/util (1).c
@@ -5,8 +5,6 @@
  * Provides utility functions for checksum generation and validation.
  *
  * Author:  Ahlaireah Lewis
- * Course:  SER486
- * Assignment: Project 2
  * Date:    4/14/2026
  * Revision: 1.0
  ********************************************************/
