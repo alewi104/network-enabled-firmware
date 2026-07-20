e5b92ce015e31a7af37d62ba12ec3e1917ea1b8c
lewi <alli.faiye@gmail.com>
2026-07-20 14:57:56 -0400

    Update header comments in vpd.h
    Removed course and assignment details from header.

--- eeprom/vpd.h
+++ eeprom/vpd.h
@@ -6,8 +6,6 @@
  * device identification data stored in EEPROM.
  *
  * Author:  Ahlaireah Lewis
- * Course:  SER486
- * Assignment: Project 2
  * Date:    4/14/2026
  * Revision: 1.0
  ********************************************************/
