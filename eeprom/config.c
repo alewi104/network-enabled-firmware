1b4ae84be616a4413f5605370fe4d8f06a661acf
lewi <alli.faiye@gmail.com>
2026-07-20 14:55:26 -0400

    Update comments in config.c
    Removed course and assignment information from comments.

--- eeprom/config.c
+++ eeprom/config.c
@@ -8,8 +8,6 @@
  * restoring factory defaults if corruption is detected.
  *
  * Author:  Ahlaireah Lewis
- * Course:  SER486
- * Assignment: Project 2
  * Date:    4/14/2026
  * Revision: 1.0
  ********************************************************/
