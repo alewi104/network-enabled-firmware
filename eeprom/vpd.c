ac815a802520f4b27acaf7e5341a0825eec8f6c2
lewi <alli.faiye@gmail.com>
2026-07-20 14:57:41 -0400

    Update comments in vpd.c
    Removed course and assignment details from comments.

--- eeprom/vpd.c
+++ eeprom/vpd.c
@@ -6,8 +6,6 @@
  * Includes validation and fallback to factory defaults.
  *
  * Author:  Ahlaireah Lewis
- * Course:  SER486
- * Assignment: Project 2
  * Date:    4/14/2026
  * Revision: 1.0
  ********************************************************/
